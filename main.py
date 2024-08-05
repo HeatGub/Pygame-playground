e55c9a28112f54e2973312d722499dbae9f6506a
HeatGub <ptrchm@proton.me>
2024-08-05 19:38:10 +0200

    draw pendulum

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import pygame
 import numpy as np
 
 pygame.init()
-WIDTH = 1000
-HEIGHT = 900
+WIDTH = 800
+HEIGHT = 600
 SCREEN = pygame.display.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption('Pygame Window')
 font = pygame.font.Font('freesansbold.ttf', 20)
@@ -12,17 +12,19 @@ font_big = pygame.font.Font('freesansbold.ttf', 50)
 clock = pygame.time.Clock()
 FPS = 60
 circleColor=[100,150,0]
+speed = 0.1
 
 def drawWindow(time):
     SCREEN.fill([0,0,5])
-    #pygame.draw.line(SCREEN, 'white', (0, 100 ), (800, 100), 2)
-    #pygame.draw.line(SCREEN, 'white', (100, 0), (100, 800), 5)
-    #pygame.display.flip() #redraws whole screen
-    x = WIDTH/2 + 300*np.sin(time/10)
-    y = HEIGHT/2 + 300*np.cos(time/10)
-    pygame.draw.circle(SCREEN, circleColor, [x, y], 30) #going in circle
-
+    x = WIDTH/2 + 200*np.sin(time*speed)
+    y = HEIGHT/2 + 200*np.cos(time*speed)
+    drawPendulum(WIDTH/2, HEIGHT/2, x, y)
     pygame.display.update()
+    #pygame.display.flip() #redraws whole screen
+
+def drawPendulum(pivotX, pivotY, x, y):
+    pygame.draw.line(SCREEN, 'white', (pivotX, pivotY), (x, y), 4)
+    pygame.draw.circle(SCREEN, circleColor, [x, y], 30)
 
 def gameLoop():
     run = True
