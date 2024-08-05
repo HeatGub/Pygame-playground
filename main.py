6a6890a30731f7922873331443bde58dda99ef09
HeatGub <ptrchm@proton.me>
2024-08-05 19:22:41 +0200

    circular motion

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
 import functions
 import pygame
+import numpy as np
 
 pygame.init()
 WIDTH = 1000
 HEIGHT = 900
-screen = pygame.display.set_mode([WIDTH, HEIGHT])
+SCREEN = pygame.display.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption('Pygame Window')
 font = pygame.font.Font('freesansbold.ttf', 20)
 font_big = pygame.font.Font('freesansbold.ttf', 50)
 clock = pygame.time.Clock()
-fps = 60
+FPS = 60
+circleColor=[100,150,0]
 
-def draw():
-    pygame.draw.line(screen, 'white', (0, 100 ), (800, 100), 2)
-    pygame.draw.line(screen, 'white', (100, 0), (100, 800), 5)
+def drawWindow(time):
+    SCREEN.fill([0,0,5])
+    #pygame.draw.line(SCREEN, 'white', (0, 100 ), (800, 100), 2)
+    #pygame.draw.line(SCREEN, 'white', (100, 0), (100, 800), 5)
+    #pygame.display.flip() #redraws whole screen
+    x = WIDTH/2 + 300*np.sin(time/10)
+    y = HEIGHT/2 + 300*np.cos(time/10)
+    pygame.draw.circle(SCREEN, circleColor, [x, y], 30) #going in circle
 
-#game loop
-run = True
-while run:
-    clock.tick(fps)
-    draw()
+    pygame.display.update()
 
-    #event handling
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-    
-    pygame.display.flip()
-pygame.quit()
+def gameLoop():
+    run = True
+    t = 0
+
+    while run:
+        clock.tick(FPS) #controls framerate
+        t = t+1
+        drawWindow(t)
+
+        #event handling
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+    pygame.quit()
+
+gameLoop()
