3ea25a9a18c0fe7b71d7f1dcc7ec9fea16c023be
HeatGub <ptrchm@proton.me>
2024-08-06 22:44:58 +0200

    moved simplePendulum to module

--- functions.py
+++ functions.py
@@ -1,3 +1,28 @@
 
-def hello():
-        print('hello')
+import random
+import numpy as np
+# from main import pygame, SCREEN, WIDTH, HEIGHT, gravity
+
+class simplePendulum:
+    def __init__(self, pivotX, pivotY, len, angle):
+        self.pivotX = pivotX
+        self.pivotY = pivotY
+        self.len = len
+        self.circleColor=[random.randint(20,200),random.randint(20,200),random.randint(20,200)]
+        self.angle = angle
+        self.angVel = 0
+        self.angAcc = 0
+
+    def update(self):
+        from main import pygame, SCREEN, WIDTH, HEIGHT, gravity
+
+        self.force = gravity * np.sin(self.angle)
+        self.angAcc = (-1 * self.force) / self.len
+        self.angVel += self.angAcc
+        self.angle += self.angVel
+
+        x = WIDTH/2 + self.len*np.sin(self.angle)
+        y = HEIGHT/2 + self.len*np.cos(self.angle)
+
+        pygame.draw.line(SCREEN, 'white', (self.pivotX, self.pivotY), (x, y), int(self.len/100))
+        pygame.draw.circle(SCREEN, self.circleColor, [x, y], self.len/10)
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 # .\.venv\Scripts\activate.bat
-#import functions
 import pygame
-import numpy as np
 import random
+import functions
 
 pygame.init()
 WIDTH = 800
@@ -13,42 +12,18 @@ font = pygame.font.Font('freesansbold.ttf', 20)
 font_big = pygame.font.Font('freesansbold.ttf', 50)
 clock = pygame.time.Clock()
 FPS = 60
-
 gravity = 1
-
-def drawWindow():
-    SCREEN.fill('black')
-    p.update()
-    for pendulum in pendulums:
-        pendulum.update()
-    pygame.display.flip() #redraws whole screen
-
-class simplePendulum:
-    def __init__(self, pivotX, pivotY, len, angle):
-        self.pivotX = pivotX
-        self.pivotY = pivotY
-        self.len = len
-        self.circleColor=[random.randint(20,200),random.randint(20,200),random.randint(20,200)]
-        self.angle = angle
-        self.angVel = 0
-        self.angAcc = 0
-
-    def update(self):
-        self.force = gravity * np.sin(self.angle)
-        self.angAcc = (-1 * self.force) / self.len
-        self.angVel += self.angAcc
-        self.angle += self.angVel
-
-        x = WIDTH/2 + self.len*np.sin(self.angle)
-        y = HEIGHT/2 + self.len*np.cos(self.angle)
-
-        pygame.draw.line(SCREEN, 'white', (self.pivotX, self.pivotY), (x, y), int(self.len/100))
-        pygame.draw.circle(SCREEN, self.circleColor, [x, y], self.len/10)
 
 pendulums = []
 for i in range(25):
-    p = simplePendulum(WIDTH/2, HEIGHT/2, random.randint(50,250), -1-3*random.random())
+    p = functions.simplePendulum(WIDTH/2, HEIGHT/2, random.randint(50,250), -1-3*random.random())
     pendulums.append(p)
+
+def drawWindow():
+    SCREEN.fill('black')
+    for pendulum in pendulums:
+        pendulum.update()
+    pygame.display.flip() #redraws whole screen
 
 def gameLoop():
     run = True
@@ -62,6 +37,7 @@ def gameLoop():
             if event.type == pygame.QUIT:
                 run = False
     pygame.quit()
+    #print(__name__)
 
 if __name__ == gameLoop(): # Execute Code When the File Runs as a Script, but Not When It's Imported as a Module
     gameLoop()
