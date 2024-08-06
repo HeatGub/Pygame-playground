2028297835cd43af67f94d307531b5f0b43ebf5d
HeatGub <ptrchm@proton.me>
2024-08-06 21:04:15 +0200

    class Pendulum added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import functions
+# .\.venv\Scripts\activate.bat
+#import functions
 import pygame
 import numpy as np
+import random
 
 pygame.init()
 WIDTH = 800
@@ -12,42 +14,48 @@ font_big = pygame.font.Font('freesansbold.ttf', 50)
 clock = pygame.time.Clock()
 FPS = 60
 
-def drawWindow(time):
+gravity = 1
+
+def drawWindow():
     SCREEN.fill('black')
-    #drawPendulum(WIDTH/2, HEIGHT/2, x, y)
-    drawPendulum(WIDTH/2, HEIGHT/2, 200)
-    #pygame.display.update()
+    p.update()
+    for pendulum in pendulums:
+        pendulum.update()
     pygame.display.flip() #redraws whole screen
 
-circleColor=[100,150,0]
-gravity = 1
-angle = np.pi/2
-angVel = 0
-angAcc = 0
+class simplePendulum:
+    def __init__(self, pivotX, pivotY, len, angle):
+        self.pivotX = pivotX
+        self.pivotY = pivotY
+        self.len = len
+        self.circleColor=[random.randint(20,200),random.randint(20,200),random.randint(20,200)]
+        self.angle = angle
+        self.angVel = 0
+        self.angAcc = 0
 
-def drawPendulum(pivotX, pivotY, len):
-    global angle
-    global angVel
-    global angAcc
-    force = gravity * np.sin(angle)
-    angAcc = (-1 * force) / len
-    angVel += angAcc
-    angle += angVel
+    def update(self):
+        self.force = gravity * np.sin(self.angle)
+        self.angAcc = (-1 * self.force) / self.len
+        self.angVel += self.angAcc
+        self.angle += self.angVel
 
-    x = WIDTH/2 + len*np.sin(angle)
-    y = HEIGHT/2 + len*np.cos(angle)
+        x = WIDTH/2 + self.len*np.sin(self.angle)
+        y = HEIGHT/2 + self.len*np.cos(self.angle)
 
-    pygame.draw.line(SCREEN, 'white', (pivotX, pivotY), (x, y), 4)
-    pygame.draw.circle(SCREEN, circleColor, [x, y], 30)
+        pygame.draw.line(SCREEN, 'white', (self.pivotX, self.pivotY), (x, y), int(self.len/100))
+        pygame.draw.circle(SCREEN, self.circleColor, [x, y], self.len/10)
+
+pendulums = []
+for i in range(25):
+    p = simplePendulum(WIDTH/2, HEIGHT/2, random.randint(50,250), -1-3*random.random())
+    pendulums.append(p)
 
 def gameLoop():
     run = True
-    t = 0
 
     while run:
         clock.tick(FPS) #controls framerate
-        t = t+1
-        drawWindow(t)
+        drawWindow()
 
         #event handling
         for event in pygame.event.get():
@@ -55,4 +63,5 @@ def gameLoop():
                 run = False
     pygame.quit()
 
-gameLoop()
+if __name__ == gameLoop(): # Execute Code When the File Runs as a Script, but Not When It's Imported as a Module
+    gameLoop()
